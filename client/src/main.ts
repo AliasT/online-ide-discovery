7edff8ff2fc69807704e449752b988df39cf4955
chaixb <1481569358@qq.com>
2019-05-29 20:37:37 +0800

    updates

---
+++ client/src/main.ts
@@ -0,0 +1,72 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+const monaco = require('monaco-editor');
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) 2018 TypeFox GmbH (http://www.typefox.io). All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+import { listen, MessageConnection } from 'vscode-ws-jsonrpc';
+const {
+    MonacoLanguageClient, CloseAction, ErrorAction,
+    MonacoServices, createConnection
+} = require('monaco-languageclient');
+const ReconnectingWebSocket = require('reconnecting-websocket');
+// create Monaco editor
+const editor = monaco.editor.create(document.getElementById('container'), {
+    value: 'print(3)',
+    language: 'python'
+  });
+
+// install Monaco language client services
+MonacoServices.install(editor);
+
+// create the web socket
+const url = "ws://localhost:3000/python"
+const webSocket = createWebSocket(url);
+// listen when the web socket is opened
+listen({
+    webSocket,
+    onConnection: connection => {
+        // create and start the language client
+        const languageClient = createLanguageClient(connection);
+        const disposable = languageClient.start();
+        connection.onClose(() => disposable.dispose());
+    }
+});
+
+function createLanguageClient(connection: MessageConnection) {
+    return new MonacoLanguageClient({
+        name: "Sample Language Client",
+        clientOptions: {
+            // use a language id as a document selector
+            documentSelector: ['python'],
+            // disable the default error handler
+            errorHandler: {
+                error: (a) => { console.log(a); return ErrorAction.Continue },
+                closed: () => CloseAction.DoNotRestart
+            }
+        },
+        // create a language client connection from the JSON RPC connection on demand
+        connectionProvider: {
+            get: (errorHandler, closeHandler) => {
+                return Promise.resolve(createConnection(connection, errorHandler, closeHandler))
+            }
+        }
+    });
+}
+
+
+function createWebSocket(url: string): WebSocket {
+    const socketOptions = {
+        maxReconnectionDelay: 10000,
+        minReconnectionDelay: 1000,
+        reconnectionDelayGrowFactor: 1.3,
+        connectionTimeout: 10000,
+        maxRetries: Infinity,
+        debug: false
+    };
+    return new ReconnectingWebSocket(url, undefined, socketOptions);
+}
+
