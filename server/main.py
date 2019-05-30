7bc6e970ff431f6893e92a48ae255ff55d512088
chaixb <1481569358@qq.com>
2019-05-30 13:35:42 +0800

    remove useless py code

--- server/main.py
+++ server/main.py
@@ -8,37 +8,6 @@ from pyls_jsonrpc import dispatchers, endpoint
 
 log = logging.getLogger(__name__)
 
-
-class LanguageServer(dispatchers.MethodDispatcher):
-    """Implement a JSON RPC method dispatcher for the language server protocol."""
-
-    def __init__(self):
-        # Endpoint is lazily set after construction
-        self.endpoint = None
-
-    def m_initialize(self, rootUri=None, **kwargs):
-        log.info("Got initialize params: %s", kwargs)
-        return {"capabilities": {
-            "textDocumentSync": {
-                "openClose": True,
-            }
-        }}
-
-    def m_text_document__did_open(self, textDocument=None, **_kwargs):
-        log.info("Opened text document %s", textDocument)
-        self.endpoint.notify('textDocument/publishDiagnostics', {
-            'uri': textDocument['uri'],
-            'diagnostics': [{
-                'range': {
-                    'start': {'line': 0, 'character': 0},
-                    'end': {'line': 1, 'character': 0},
-                },
-                'message': 'Some very bad Python code',
-                'severity': 1  # DiagnosticSeverity.Error
-            }]
-        })
-
-
 class LanguageServerWebSocketHandler(websocket.WebSocketHandler):
     """Setup tornado websocket handler to host language server."""
 
